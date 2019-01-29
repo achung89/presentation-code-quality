7b703d03a6169e00c6e9758cee0e4d63d20c6483
Andrew Chung <chung.andrew7@gmail.com>
2019-01-29 07:00:50 -0500

    added some slides

--- src/index.js
+++ src/index.js
@@ -21,29 +21,45 @@ import {
   Text
 } from "spectacle";
 import createTheme from "spectacle-theme-nova";
-
+import timeVProductivity from "./img/productivity-vs-time.png";
 const theme = createTheme();
 export default class App extends Component {
   render() {
     return (
       <Deck theme={theme}>
         <Slide>
-          <Heading>Code Quality (the basics)</Heading>
+          <Heading>Code Quality</Heading>
         </Slide>
         <Slide>
-          <Heading>What is Code Quality</Heading>
+          <Heading>Good Code</Heading>
         </Slide>
         <Slide>
-          <Heading>Definitions of code quality</Heading>
+          <img src={timeVProductivity} />
+        </Slide>
+        <Slide>
+          <Heading>Good code...</Heading>
+          <List>
+            <ListItem>Increases productivity</ListItem>
+            <ListItem>Decreases bugs</ListItem>
+            <ListItem>Makes life easier for programmers</ListItem>
+            <ListItem>Good for everyone!</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading>Quantifying code quality</Heading>
+          <img src={require('./img/wtfm.jpg')} />
+        </Slide>
+        <Slide>
+          <Heading>
+            Code is communication
+          </Heading>
           <List>
             <ListItem>
-              Code quality is a measure of how easy it is to add to or edit a piece of code without breaking anything
-            </ListItem>
-            <ListItem>
-              Code quality is a measure of how long a piece of code will last before becoming unmaintainable
+              "Code is not for the computer, it's for other human beings" - Kyle Simpson
             </ListItem>
           </List>
         </Slide>
+        "If you go after easy, you usually end up with complex, but if you go after simple you can also achieve easy." - Kyle Simpson
 
       </Deck>
     );
